cbeb8987a9cc61bbc0233213751cbaa772d46fc9
Didier <dyder1493@gmail.com>
2021-03-01 13:06:28 +0100

    REdux

---
+++ expensify-app/src/playground/redux_basic.js
@@ -0,0 +1,55 @@
+import { createStore } from 'redux';
+
+const increment = ({ incrementBy = 1 } = {}) => ({
+        type: 'INCREMENT',
+        incrementBy
+});
+
+const decrement = ({ decrementBy = 1 } = {}) => ({
+    type: 'DECREMENT',
+    decrementBy
+});
+
+const reset = () => ({
+    type: 'RESET'
+});
+
+const setCount = ({randomInt = 1000}) => ({ //force user to set a count
+    type: 'TEST',
+    randomInt: randomInt
+});
+
+const store = createStore((state = { counter: 0 }, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                counter: state.counter + action.incrementBy
+            }
+        case 'DECREMENT':
+            return {
+                counter: state.counter - action.decrementBy
+            }
+        case 'RESET':
+            return {
+                counter: 0
+            }
+        case 'TEST':
+            return {
+                counter: action.randomInt
+            }
+    }
+});
+
+store.subscribe(() => { // est appelé à chaque changement de state
+    console.log(store.getState());
+});
+
+store.dispatch(increment());
+
+store.dispatch(increment({incrementBy: 5}));
+
+store.dispatch(decrement());
+
+store.dispatch(reset());
+
+store.dispatch(setCount({randomInt:2}));
